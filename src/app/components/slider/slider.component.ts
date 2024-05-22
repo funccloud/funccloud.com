975d11039d219129f0b5520c9707cd9cc4f90831
Felipe Oliveira <fpo@felipeweb.dev>
2024-05-22 13:08:31 -0300

    angular
    Signed-off-by: Felipe Oliveira <fpo@felipeweb.dev>

---
+++ src/app/components/slider/slider.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slider',
+  standalone: true,
+  imports: [],
+  templateUrl: './slider.component.html',
+  styleUrl: './slider.component.scss',
+  host: { ngSkipHydration: 'true' },
+})
+export class SliderComponent {
+
+}
