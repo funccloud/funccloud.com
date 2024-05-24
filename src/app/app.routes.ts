5277d2496c52cd4ffe5a032442ca4e060cfa4be7
Felipe Oliveira <fpo@felipeweb.dev>
2024-05-23 22:28:01 -0300

    title
    Signed-off-by: Felipe Oliveira <fpo@felipeweb.dev>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,14 @@ import { IndexComponent } from './pages/index/index.component';
 import { GwsComponent } from './pages/gws/gws.component';
 
 export const routes: Routes = [
-  { path: '', component: IndexComponent },
-  { path: 'solucoes/produtividade-colaboracao/google-workspace', component: GwsComponent },
+  {
+    path: '',
+    component: IndexComponent,
+    title: 'Funccloud Soluções Tecnológicas'
+  },
+  {
+    path: 'solucoes/produtividade-colaboracao/google-workspace',
+    component: GwsComponent,
+    title: 'Funccloud Soluções Tecnológicas | Google Workspace'
+  },
 ];
