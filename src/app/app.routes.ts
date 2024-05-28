4bdfdc05f9604c591a0aaec41ec32afb68fbf44a
Felipe Oliveira <fpo@felipeweb.dev>
2024-05-27 22:24:10 -0300

    posture review
    Signed-off-by: Felipe Oliveira <fpo@felipeweb.dev>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { IndexComponent } from './pages/index/index.component';
 import { GwsComponent } from './pages/gws/gws.component';
 import { ContatoComponent } from './pages/contato/contato.component';
+import { PostureReviewComponent } from './pages/posture-review/posture-review.component';
 
 export const routes: Routes = [
   {
@@ -18,5 +19,10 @@ export const routes: Routes = [
     path: 'solucoes/produtividade-colaboracao/google-workspace',
     component: GwsComponent,
     title: 'FuncCloud - Facilitamos a Inovação na Nuvem | Google Workspace'
+  },
+  {
+    path: 'solucoes/cloud-services/posture-review',
+    component: PostureReviewComponent,
+    title: 'FuncCloud - Facilitamos a Inovação na Nuvem | Posture Review (Segurança & Compliance)'
   },
 ];
