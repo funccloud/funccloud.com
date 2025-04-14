1de991be80000e6c4a13880a9383326e5a91c904
Felipe Oliveira <felipe@funccloud.com>
2025-04-14 20:11:34 -0300

    locale

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app/app.component';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 
-registerLocaleData(localePt)
+registerLocaleData(localePt);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
