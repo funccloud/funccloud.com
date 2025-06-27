f9fc930dc5c63939daed6bceb977e59a00283cbd
Felipe Oliveira <felipe@funccloud.com>
2025-06-26 23:25:42 -0300

    fix prerender
    Signed-off-by: Felipe Oliveira <felipe@funccloud.com>

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,13 +1,26 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 import { routes } from './app.routes';
+import { postSlugs } from './services/post-slugs.generated';
 
 const serverRoutes: ServerRoute[] = routes
   .filter((route) => route.path !== '**')
-  .map((route) => ({
-    path: route.path!,
-    renderMode: RenderMode.Prerender,
-    status: 200,
-  }));
+  .map((route) => {
+    if (route.path === 'blog/:slug') {
+      return {
+        path: route.path,
+        renderMode: RenderMode.Prerender,
+        status: 200,
+        getPrerenderParams() {
+          return postSlugs.map((slug) => ({ slug })); // Format for prerendering
+        },
+      };
+    }
+    return {
+      path: route.path!,
+      renderMode: RenderMode.Prerender,
+      status: 200,
+    };
+  });
 
 serverRoutes.push({
   path: '**',
