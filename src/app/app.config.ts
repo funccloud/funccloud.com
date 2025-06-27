1069591de7897a42fd03f8d515e7ce2a54d1e531
Felipe Oliveira <felipe@funccloud.com>
2025-06-26 22:10:58 -0300

    angular 20
    Signed-off-by: Felipe Oliveira <felipe@funccloud.com>

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, LOCALE_ID, provideExperimentalZonelessChangeDetection, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZonelessChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http'; // Importar withFetch
@@ -7,7 +7,7 @@ import { provideMarkdown } from 'ngx-markdown'; // Importar provideMarkdown
 export const appConfig: ApplicationConfig = {
   providers: [
     { provide: LOCALE_ID, useValue: 'pt-BR' },
-    provideExperimentalZonelessChangeDetection(),
+    provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(withFetch()), // Habilitar fetch API para HttpClient
     provideMarkdown(), // Adicionar o provider do ngx-markdown
