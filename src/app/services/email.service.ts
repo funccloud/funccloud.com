d57c7fb3ed0586a9665d3c72feeadf80107d2a5e
Felipe Oliveira <felipe@funccloud.com>
2025-08-13 21:11:38 -0300

    fix-form
    Signed-off-by: Felipe Oliveira <felipe@funccloud.com>

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -4,21 +4,28 @@ import { environment } from '../../environments/environment';
 import { lastValueFrom } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmailService {
-  constructor(private http: HttpClient) { }
-  sendEmail(to: string, subject: string, template: string, content: Record<string, unknown>): Promise<any> {
+  constructor(private http: HttpClient) {}
+  sendEmail(
+    to: string,
+    subject: string,
+    template: string,
+    content: Record<string, unknown>
+  ): Promise<any> {
     let body = JSON.stringify({
-      from: "FuncCloud <noreply@funccloud.com>",
+      from: 'FuncCloud <noreply@funccloud.com>',
       to: to,
       subject: subject,
       parameters: content,
     });
-    return lastValueFrom(this.http.post(`/.netlify/functions/emails/${template}`, body, {
-      headers: {
-        "netlify-emails-secret": environment.envVar.NETLIFY_EMAILS_SECRET
-      }
-    }));
+    return lastValueFrom(
+      this.http.post(`/.netlify/functions/emails/${template}`, body, {
+        headers: {
+          'netlify-emails-secret': environment.envVar.NETLIFY_EMAILS_SECRET,
+        },
+      })
+    );
   }
 }
